062da4b5d527dd7640a3bf2a2cae97655e92866e
Pathan Arshlan <141414515+Arshlankhan786@users.noreply.github.com>
2024-12-30 23:39:07 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+API_ID = 'your_api_id'  # Your Telegram API ID
+API_HASH = 'your_api_hash'  # Your Telegram API hash
+BOT_TOKEN = 'your_bot_token'  # Your Telegram bot token
+
+MONGO_URI = 'mongodb://localhost:27017'  # MongoDB URI for user data storage
+DB_NAME = 'file_management_bot_db'  # MongoDB database name
+
+TEMP_DOWNLOAD_PATH = '/path/to/temp/download'  # Path to store temporary files
+MAX_FILE_SIZE_MB = 2000  # Maximum file size in MB for upload
