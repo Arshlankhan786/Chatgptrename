a503d56a5b1820cf505c232d9f5840a9cfe7cf75
Pathan Arshlan <141414515+Arshlankhan786@users.noreply.github.com>
2024-12-30 23:57:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-API_ID = 'your_api_id'  # Your Telegram API ID
-API_HASH = 'your_api_hash'  # Your Telegram API hash
-BOT_TOKEN = 'your_bot_token'  # Your Telegram bot token
+API_ID = '26058820'  # Your Telegram API ID
+API_HASH = 'cf34db4ed69493d377a2516056711cb6'  # Your Telegram API hash
+BOT_TOKEN = '6910550441:AAHc1UaULbAUD-iUMh4HpAaCzY8MBt6bdn4'  # Your Telegram bot token
 
-MONGO_URI = 'mongodb://localhost:27017'  # MongoDB URI for user data storage
+MONGO_URI = 'mongodb+srv://Order:order@cluster0.aitjsft.mongodb.net/?retryWrites=true&w=majority'  # MongoDB URI for user data storage
 DB_NAME = 'file_management_bot_db'  # MongoDB database name
 
 TEMP_DOWNLOAD_PATH = '/path/to/temp/download'  # Path to store temporary files
